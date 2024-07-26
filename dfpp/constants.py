29451364660db0fc6f3416d3972e9eabada9f0cc
Mykola Skrynnyk <45297092+mykolaskrynnyk@users.noreply.github.com>
2024-07-26 17:21:15 +0200

    Refactor(dfpp/constants): create paths from environment variable

--- dfpp/constants.py
+++ dfpp/constants.py
@@ -1,14 +1,15 @@
-import datetime
+import os
+from datetime import datetime, timezone
 
+ROOT_FOLDER = os.getenv("ROOT_FOLDER")
 STANDARD_KEY_COLUMN = 'Alpha-3 code'
 STANDARD_COUNTRY_COLUMN = 'Country'
-COUNTRY_LOOKUP_CSV_PATH = "DataFuturePlatform/pipeline/config/utilities/country_lookup.xlsx"
-SOURCE_CONFIG_ROOT_FOLDER = "DataFuturePlatform/pipeline/config/sources"
-INDICATOR_CONFIG_ROOT_FOLDER = "DataFuturePlatform/pipeline/config/indicators"
-OUTPUT_FOLDER = "DataFuturePlatform/pipeline/output"
+COUNTRY_LOOKUP_CSV_PATH = f"{ROOT_FOLDER}/config/utilities/country_lookup.xlsx"
+SOURCE_CONFIG_ROOT_FOLDER = f"{ROOT_FOLDER}/config/sources"
+INDICATOR_CONFIG_ROOT_FOLDER = f"{ROOT_FOLDER}/config/indicators"
+OUTPUT_FOLDER = f"{ROOT_FOLDER}/output"
 POOL_COMMAND_TIMEOUT = 15 * 60  # seconds
 POOL_MINSIZE = 3
 POOL_MAXSIZE = 50
 CONNECTION_TIMEOUT = 30
-TODAY = datetime.date.today()
-CURRENT_YEAR = TODAY.year
+CURRENT_YEAR = datetime.now(timezone.utc).year
