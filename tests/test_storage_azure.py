c177ed29cfab59d5965e470258554ef2bca7687f
Mykola Skrynnyk <45297092+mykolaskrynnyk@users.noreply.github.com>
2024-08-08 14:14:23 +0200

    105 refactor move tests outside the package (#106)
    * Test: remove obsolete tests
    
    * Build: add development dependencies
    
    * Test: add basic unit tests for `azure.py` module in `storage` subpackage

---
+++ tests/test_storage_azure.py
@@ -0,0 +1,66 @@
+"""
+Unit tests for Azure module in Store subpackage that implements an interface for Azure Blob Storage.
+"""
+
+import json
+
+import pytest
+
+from dfpp.storage.azure import StorageManager
+
+
+@pytest.mark.asyncio
+async def test_client_connection():
+    async with StorageManager() as storage_manager:
+        # not possible to check with container-level SAS, see
+        # https://github.com/Azure/azure-sdk-for-python/issues/18012#issuecomment-1146435699
+        # assert await storage_manager.container_client.exists(), "Container does not exist"
+        blob_client = storage_manager.container_client.get_blob_client("foo.bar")
+        assert isinstance(await blob_client.exists(), bool), "Failed "
+
+
+@pytest.mark.parametrize(
+    "kind",
+    ["indicators", "sources"],
+)
+@pytest.mark.asyncio
+async def test_list_configs(kind: str):
+    async with StorageManager() as storage_manager:
+        configs = await storage_manager.list_configs(kind=kind)
+        assert isinstance(configs, list), "Unexpected return type"
+        assert len(configs) > 0, "Unexpected number of items"
+        assert isinstance(configs[0], str), "Unexpected return item type"
+
+
+@pytest.mark.parametrize(
+    "content",
+    ["foo", '{"bar": "baz"}'],
+)
+@pytest.mark.asyncio
+async def test_upload_blob_from_file(content: str, tmp_path):
+    path_src = tmp_path / "test.txt"
+    path_src.write_text(content)
+    async with StorageManager() as storage_manager:
+        path = await storage_manager.upload_blob(
+            path_or_data_src=str(path_src),
+            path_dst="tests/qux.txt"
+        )
+        assert isinstance(path, str), "Unexpected return type"
+        data = await storage_manager.read_blob(path)
+        assert content == data.decode(), "Content mismatch"
+
+
+@pytest.mark.parametrize(
+    "content",
+    ["foo", json.dumps({"bar": "baz"})],
+)
+@pytest.mark.asyncio
+async def test_upload_blob_from_data(content: str | dict, tmp_path):
+    async with StorageManager() as storage_manager:
+        path = await storage_manager.upload_blob(
+            path_or_data_src=content.encode(),
+            path_dst="tests/qux.txt"
+        )
+        assert isinstance(path, str), "Unexpected return type"
+        data = await storage_manager.read_blob(path)
+        assert content == data.decode(), "Content mismatch"
