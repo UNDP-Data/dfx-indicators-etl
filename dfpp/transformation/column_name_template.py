a4773f4e240d2689902956132630a6ce93ed792a
mykhailo.slukvin <mykhailo.slukvin@gmail.com>
2024-09-22 21:54:25 +0300

    store basic column name and column value conventions

---
+++ dfpp/transformation/column_name_template.py
@@ -0,0 +1,10 @@
+"""store basic column name and column value conventions"""
+
+from enum import StrEnum, Enum
+
+dimension_canonical_names = ["sex", "year", "age", "alpha_3_code", "country_or_area", "education"]
+
+class SexEnum(StrEnum, Enum):
+    MALE = "male"
+    FEMALE = "female"
+    BOTH = "both"
