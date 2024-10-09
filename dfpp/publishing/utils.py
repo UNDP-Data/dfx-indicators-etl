48de538841c1f9e00c65bc1c3afd6537741e2630
mykhailo.slukvin <mykhailo.slukvin@gmail.com>
2024-10-09 15:19:02 +0300

    publish update - leave only necessary parts to publisht transformed series

--- dfpp/publishing/utils.py
+++
@@ -1,9 +0,0 @@
-import itertools
-
-def chunker(iterable, size):
-    it = iter(iterable)
-    while True:
-        chunk = tuple(itertools.islice(it, size))
-        if not chunk:
-            break
-        yield chunk
