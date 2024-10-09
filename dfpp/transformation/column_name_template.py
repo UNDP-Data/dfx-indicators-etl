4629b052e28336039978bbcaf0eb32e8d46835d9
mykhailo.slukvin <mykhailo.slukvin@gmail.com>
2024-10-09 15:19:34 +0300

    update column name constants

--- dfpp/transformation/column_name_template.py
+++ dfpp/transformation/column_name_template.py
@@ -2,7 +2,8 @@
 
 from enum import StrEnum, Enum
 
-dimension_canonical_names = ["sex", "year", "age", "alpha_3_code", "country_or_area", "education"]
+CANONICAL_COLUMN_NAMES= ["alpha_3_code", "country_or_area", "year", "value", "series_id", "source"]
+
 
 class SexEnum(StrEnum, Enum):
     MALE = "male"
@@ -10,3 +11,28 @@ class SexEnum(StrEnum, Enum):
     BOTH = "both"
     OTHER = "other"
     TOTAL = "total"
+    NOT_APPLICABLE = "not applicable"
+
+
+def sort_columns_canonically(df):
+    """
+    Sort the columns of a DataFrame so that the canonical columns appear first, 
+    in the specified order. Other columns remain in the DataFrame in their original order.
+
+    Args:
+        df (pd.DataFrame): The input DataFrame.
+
+    Returns:
+        pd.DataFrame: A new DataFrame with the columns sorted.
+    """
+    assert all(col in df.columns for col in CANONICAL_COLUMN_NAMES), \
+    f"DataFrame does not contain all canonical columns. Missing columns: {set(CANONICAL_COLUMN_NAMES) - set(df.columns)}"
+
+    canonical_cols_start = [col for col in CANONICAL_COLUMN_NAMES if col in df.columns][:2]
+    canonical_cols_end = [col for col in CANONICAL_COLUMN_NAMES if col in df.columns][2:]
+    
+    other_cols = [col for col in df.columns if col not in CANONICAL_COLUMN_NAMES]
+    
+    sorted_columns = canonical_cols_start + other_cols + canonical_cols_end
+    
+    return df[sorted_columns]
