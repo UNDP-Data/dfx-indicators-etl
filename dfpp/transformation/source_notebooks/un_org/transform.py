c72bd7213ad4e598c059c8a1f27e61f0e7a7fa7d
mykhailo.slukvin <mykhailo.slukvin@gmail.com>
2024-10-23 01:22:32 +0300

    un sdgi api: store dimension codes and  human readable names

--- dfpp/transformation/source_notebooks/un_org/transform.py
+++ dfpp/transformation/source_notebooks/un_org/transform.py
@@ -2,7 +2,7 @@
 
 import pandas as pd
 
-from dfpp.transformation.column_name_template import DIMENSION_COLUMN_PREFIX, SexEnum, sort_columns_canonically
+from dfpp.transformation.column_name_template import DIMENSION_COLUMN_PREFIX, DIMENSION_COLUMN_CODE_SUFFIX, DIMENSION_COLUMN_NAME_SUFFIX, SexEnum, sort_columns_canonically
 
 from dfpp.transformation.source_notebooks.un_org.retrieve import BASE_URL
 
@@ -33,67 +33,55 @@ def transform_series(
     df_attribute_codebook: pd.DataFrame = None,
     iso_3_map_numeric_to_alpha: dict = None,
 ) -> pd.DataFrame:
-    """transform a signle series dataframe to the publishable format
-    Args:
-        series_id (str): the series id
-        df (pd.DataFrame): the dataframe to transform
-        df_dimension_codebook (pd.DataFrame): the dimension column codebook
-        df_attribute_codebook (pd.DataFrame): the attributes codebook
-        iso_3_map_numeric_to_alpha (dict): the iso_3_map_numeric_to_alpha to remap numeric country names to alpha iso3
-    Returns:
-        pd.DataFrame: the transformed dataframe
-    """
     df_dimensions = pd.json_normalize(df["dimensions"])
     df_attributes = pd.json_normalize(df["attributes"])
     
     df = pd.concat([df.drop(columns=["dimensions", "attributes"]), df_dimensions, df_attributes], axis=1)
 
     dimension_columns = df_dimension_codebook["id"].unique().tolist()
-    dimension_columns_formatted = [column.lower().replace(" ", "_") for column in dimension_columns]
 
     for column in dimension_columns:
-        if column in df.columns:
-            to_remap = dict(df_dimension_codebook[df_dimension_codebook["id"] == column][["code", "description"]].values)
-            df[column] = df[column].replace(to_remap)
-    
-    for column in df_attribute_codebook["id"].unique().tolist():
-        if column in df.columns:
-            to_remap = dict(df_attribute_codebook[df_attribute_codebook["id"] == column][["code", "description"]].values)
-            df[column] = df[column].replace(to_remap)
+        if not column in df.columns:
+            continue
 
-    dimension_column_rename_map = dict(
-        zip(dimension_columns, dimension_columns_formatted)
-    )
+        to_remap = dict(df_dimension_codebook[df_dimension_codebook["id"] == column][["code", "description"]].values)
+        column_name_formatted = column.lower().replace(" ", "_")
+
+        if column_name_formatted == "age":
+            df[f"{DIMENSION_COLUMN_PREFIX}{column_name_formatted}{DIMENSION_COLUMN_CODE_SUFFIX}"] = df[column]
+            df[f"{DIMENSION_COLUMN_PREFIX}{column_name_formatted}{DIMENSION_COLUMN_NAME_SUFFIX}"] = df[column].replace(AGE_REMAP)
+            continue
+        if column_name_formatted == "sex":
+            df[f"{DIMENSION_COLUMN_PREFIX}{column_name_formatted}{DIMENSION_COLUMN_CODE_SUFFIX}"] = df[column]
+            df[f"{DIMENSION_COLUMN_PREFIX}{column_name_formatted}{DIMENSION_COLUMN_NAME_SUFFIX}"] = df[column].replace(SEX_REMAP)
+            continue
+
+        df[f"{DIMENSION_COLUMN_PREFIX}{column_name_formatted}{DIMENSION_COLUMN_CODE_SUFFIX}"] = df[column]
+        df[f"{DIMENSION_COLUMN_PREFIX}{column_name_formatted}{DIMENSION_COLUMN_NAME_SUFFIX}"] = df[column].replace(to_remap)
+        
+
+    for column in df_attribute_codebook["id"].unique().tolist():
+        if not column in df.columns:
+            continue
+        to_remap = dict(df_attribute_codebook[df_attribute_codebook["id"] == column][["code", "description"]].values)
+        df[column] = df[column].replace(to_remap)
 
     columns_to_rename = PRIMARY_COLUMNS_TO_RENAME.copy()
-    columns_to_rename.update(dimension_column_rename_map)
 
-    df.rename(
-        columns=columns_to_rename,
-        inplace=True,
-    )
+    df.rename(columns=columns_to_rename, inplace=True)
 
+    disagr_columns = [col for col in df.columns if col.startswith(DIMENSION_COLUMN_PREFIX)]
     df_selection = df.copy()[
         ["series_name", "alpha_3_code", "year", "value", "unit", "observation_type"]
-        + dimension_columns_formatted
+        + disagr_columns
     ]
     df_selection["alpha_3_code"] = df_selection["alpha_3_code"].astype(int)
     df_selection["alpha_3_code"] = df_selection["alpha_3_code"].replace(iso_3_map_numeric_to_alpha)
-
-    if "age" in df.columns:
-        df_selection["age"] = df_selection["age"].replace(AGE_REMAP)
-
-    if "sex" in df.columns:
-        df_selection["sex"] = df_selection["sex"].replace(SEX_REMAP)
-
-    df_selection.columns = [
-        DIMENSION_COLUMN_PREFIX + col if col in dimension_columns_formatted else col
-        for col in df_selection.columns
-    ]
 
     df_selection["source"] = BASE_URL
     df_selection["series_id"] = series_id
 
     df_selection = sort_columns_canonically(df_selection)
-    assert df_selection.drop("value", axis=1).duplicated().sum() == 0, "Duplicate rows per country year found after transformation, make sure that any dimension columns are not omitted from the transformed data."
+    assert df_selection.drop("value", axis=1).duplicated().sum() == 0
+
     return df_selection
