af51cba521dd1b7082046d79a8aada3d54f54a60
mykhailo.slukvin <mykhailo.slukvin@gmail.com>
2024-10-30 00:03:25 +0200

    feat: update column template - unit and observation type fields are now treated as dimensions rather than properties

--- dfpp/transformation/column_name_template.py
+++ dfpp/transformation/column_name_template.py
@@ -1,15 +1,25 @@
 """store basic column name and column value conventions"""
+
 import re
 import pandas as pd
 from enum import StrEnum, Enum
 
-CANONICAL_COLUMN_NAMES= ["source", "series_id", "series_name", "alpha_3_code",
-                          "year", "value", "unit", "observation_type"]
-
-
 DIMENSION_COLUMN_PREFIX = "disagr_"
 DIMENSION_COLUMN_CODE_SUFFIX = "_code"
 DIMENSION_COLUMN_NAME_SUFFIX = "_name"
+
+CANONICAL_COLUMN_NAMES = [
+    "source",
+    "series_id",
+    "series_name",
+    "alpha_3_code",
+    "year",
+    "value",
+    DIMENSION_COLUMN_PREFIX + "unit" + DIMENSION_COLUMN_CODE_SUFFIX,
+    DIMENSION_COLUMN_PREFIX + "unit" + DIMENSION_COLUMN_NAME_SUFFIX,
+    DIMENSION_COLUMN_PREFIX + "observation_type" + DIMENSION_COLUMN_CODE_SUFFIX,
+    DIMENSION_COLUMN_PREFIX + "observation_type" + DIMENSION_COLUMN_NAME_SUFFIX,
+]
 
 
 class SexEnum(StrEnum, Enum):
@@ -24,26 +34,44 @@ class SexEnum(StrEnum, Enum):
 def set_dimension_column_prefix(dimension_columns: str):
     return dimension_columns
 
-def sort_columns_canonically(df):
-    assert all(col in df.columns for col in CANONICAL_COLUMN_NAMES), \
-    f"DataFrame does not contain all canonical columns. Missing columns: {set(CANONICAL_COLUMN_NAMES) - set(df.columns)}"
 
-    canonical_cols_start = [col for col in CANONICAL_COLUMN_NAMES if col in df.columns][:4]
-    canonical_cols_end = [col for col in CANONICAL_COLUMN_NAMES if col in df.columns][4:]
+def sort_columns_canonically(df):
+    assert all(
+        col in df.columns for col in CANONICAL_COLUMN_NAMES
+    ), f"DataFrame does not contain all canonical columns. Missing columns: {set(CANONICAL_COLUMN_NAMES) - set(df.columns)}"
+
+    canonical_cols_start = [col for col in CANONICAL_COLUMN_NAMES if col in df.columns][
+        :4
+    ]
+    canonical_cols_end = [col for col in CANONICAL_COLUMN_NAMES if col in df.columns][
+        4:
+    ]
 
     grouped_disagr_cols = get_grouped_disagr_columns(df)
 
-    other_cols = [col for col in df.columns if col not in CANONICAL_COLUMN_NAMES and col not in grouped_disagr_cols]
+    other_cols = [
+        col
+        for col in df.columns
+        if col not in CANONICAL_COLUMN_NAMES and col not in grouped_disagr_cols
+    ]
 
-    sorted_columns = canonical_cols_start + grouped_disagr_cols + other_cols + canonical_cols_end
+    sorted_columns = (
+        canonical_cols_start + grouped_disagr_cols + other_cols + canonical_cols_end
+    )
 
     return df[sorted_columns]
 
 
 def get_grouped_disagr_columns(df: pd.DataFrame) -> list:
     """Returns the grouped disagr columns in the DataFrame
-      so that _code and _name suffixed dimension columns are displayed next to each other."""
-    disagr_cols = [col for col in df.columns if col.startswith("disagr_")]
+    so that _code and _name suffixed dimension columns are displayed next to each other.
+    """
+    disagr_cols = [
+        col
+        for col in df.columns
+        if col.startswith("disagr_") and col not in CANONICAL_COLUMN_NAMES
+    ]
+    
     if not disagr_cols:
         return []
 
