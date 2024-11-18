4950de822566a46256a7ce1001d82ef2c0fbd483
mykhailo.slukvin <mykhailo.slukvin@gmail.com>
2024-11-18 02:08:51 +0200

    formatting

--- dfpp/transformation/value_handler.py
+++ dfpp/transformation/value_handler.py
@@ -11,25 +11,31 @@ class ValueLabel(Enum):
     NEGATIVE_INFINITY = -99999999
     STRING_ONLY = "string"
 
+
 def handle_value(row: pd.Series) -> tuple[str | float, str]:
     """Process a row of data and return a tuple of the processed value and its label.
     Indicating whether value conversion to float64 can be done."""
-    value = row['value']
+    value = row["value"]
 
     def is_pure_zero_prefixed_number(string):
         """Check if the string is zero-prefixed and does not contain ',' or '.'."""
-        return re.match(r"0[0-9]+$", string) and not any(c in string for c in [",", "."])
+        return re.match(r"0[0-9]+$", string) and not any(
+            c in string for c in [",", "."]
+        )
 
     def try_convert_to_float(string_value):
         """Attempt to convert a string to a float after replacing ',' with '.'."""
         try:
-            return float(string_value.replace(',', '.')), ValueLabel.STRING_TO_FLOAT.value
+            return (
+                float(string_value.replace(",", ".")),
+                ValueLabel.STRING_TO_FLOAT.value,
+            )
         except ValueError:
             return None
 
     def handle_comparison_string(value):
         """Handle cases where the string contains comparison operators."""
-        match = re.match(r'(-?[0-9,\.]+)?\s*([<>]=?|>>|<<)\s*(-?[0-9,\.]+)?', value)
+        match = re.match(r"(-?[0-9,\.]+)?\s*([<>]=?|>>|<<)\s*(-?[0-9,\.]+)?", value)
         if match:
             pre_value = match.group(1)
             operator = match.group(2) if match.group(2) else ""
@@ -47,14 +53,14 @@ def handle_value(row: pd.Series) -> tuple[str | float, str]:
                         return float_value, ValueLabel.POSITIVE_INFINITY.value
                     if "<" in operator:
                         return float_value, ValueLabel.NEGATIVE_INFINITY.value
-                    
+
                     return float_value, label
 
         return value, ValueLabel.STRING_ONLY.value
 
     def handle_numeric_string(value):
         """Extract and convert the numeric part of a string."""
-        match = re.search(r'\d+(?:[.,]\d+)*', value)
+        match = re.search(r"\d+(?:[.,]\d+)*", value)
         if match:
             string_value = match.group(0)
             if is_pure_zero_prefixed_number(string_value):
