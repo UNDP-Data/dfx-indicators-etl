a4423f2ee29c81fb8046d21d463f23af46bd55c6
mykhailo.slukvin <mykhailo.slukvin@gmail.com>
2024-11-26 23:03:26 +0200

    feat: coerce strings to floats chore: update documentation of the value handler refact: simplify conditions for the string coercion to float

--- dfpp/transformation/value_handler.py
+++ dfpp/transformation/value_handler.py
@@ -1,30 +1,35 @@
 import re
 import pandas as pd
-from enum import Enum
 
-
-class ValueLabel(Enum):
-    POSITIVE_INFINITY = 99999999
-    NEGATIVE_INFINITY = -99999999
 
 def handle_value(row: pd.Series) -> tuple[str | float, str]:
-    """Process a row of data and return a tuple of the processed value and its label.
-    Indicating whether value conversion to float64 can be done."""
+    """Process a row to coerce strings to numeric.
+    If string value cannot be coerced return None
+
+    Args:
+        row (pd.Series): row of the indicator series to process, must contain `value` colum
+
+    Returns:
+        tuple[str | float, str]:
+        value: None | float | int - value coerced to float,
+        label:  None | str - label for values that contain comparision sign (original string value returned in such cases)
+    """
     value = row["value"]
 
     def is_pure_zero_prefixed_number(value):
         """Check if the string is zero-prefixed and does not contain float separator"""
         return bool(re.match(r"0[0-9]*(\.[0-9]+)?$", value)) and not "." in value
-    
+
     def try_convert_to_float(value):
-        """Attempt to convert a string to a float after replacing ',' with '.'"""
-        if re.search(r"\d[\+\-\*/]\d", value):
-            return value
+        """Attempt to convert a string to a float after replacing ',' with empty string"""
         try:
+            # substitute percentage symbol with empty string
             value = re.sub(r"\s*%\s*", "", value)
+
+            # replace comma with empty string
             return float(value.replace(",", ""))
         except ValueError:
-            return value
+            return None
 
     def handle_comparison_string(value):
         """
@@ -32,31 +37,27 @@ def handle_value(row: pd.Series) -> tuple[str | float, str]:
         Replace commas with "" if present. If alphabet symbols or invalid characters
         are present, return the original value. Handles inequality signs appropriately.
         """
-        if re.search(r"[a-zA-Z]", value):
-            return value, None
-
+        # remove comparison operators before converting
         cleaned_value = re.sub(r"[<>]=?", "", value)
         float_value = try_convert_to_float(cleaned_value)
-        
-        if ">" in value:
-            return (float_value, ValueLabel.POSITIVE_INFINITY)
 
-        if "<" in value:
-            return (float_value, ValueLabel.NEGATIVE_INFINITY)
-        return float_value, None
-    
+        if float_value is not None:
+            return (float_value, value)
+
+        return None, None
+
     if isinstance(value, (int, float)):
         return value, None
 
     if isinstance(value, str):
         if is_pure_zero_prefixed_number(value):
-            return value, None
+            return None, None
 
         if any(c in value for c in ["<", ">"]):
             return handle_comparison_string(value)
 
-        numeric_result = try_convert_to_float(value)
-        
-        return numeric_result, None
+        float_value = try_convert_to_float(value)
 
-    return value, None
+        return float_value, None
+
+    return None, None
