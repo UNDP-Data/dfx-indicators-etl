8947d0f0942e6aa9dc3e0c076c13316bdc493253
mykhailo.slukvin <mykhailo.slukvin@gmail.com>
2024-12-03 23:24:44 +0200

    feat: add transform for worldbank api

---
+++ dfpp/sources/world_bank/transform.py
@@ -0,0 +1,113 @@
+"""
+transform series data retrieved via api into publishable format
+"""
+
+import logging
+import pandas as pd
+
+from dfpp.transformation.column_name_template import (
+    sort_columns_canonically,
+    ensure_canonical_columns,
+    SERIES_PROPERTY_PREFIX,
+    DIMENSION_COLUMN_PREFIX,
+    CANONICAL_COLUMN_NAMES,
+)
+from dfpp.transformation.value_handler import handle_value
+from dfpp.transformation import exceptions
+
+from dfpp.sources.world_bank.retrieve import BASE_URL
+
+logging.basicConfig(
+    level=logging.INFO,
+    format="%(asctime)s - %(levelname)s - %(module)s - %(message)s",
+    handlers=[logging.StreamHandler()],
+)
+
+
+PRIMARY_COLUMNS_TO_RENAME = {
+    "unit": SERIES_PROPERTY_PREFIX + "unit",
+    "obs_status": SERIES_PROPERTY_PREFIX + "observation_type",
+    "date": "year",
+    "value": "value",
+    "value_label": SERIES_PROPERTY_PREFIX + "value_label",
+}
+
+__all__ = ["transform_series"]
+
+
+def transform_series(df: pd.DataFrame, iso_3_map: dict) -> pd.DataFrame:
+    """
+    Transform a DataFrame containing series data into a structured format.
+
+    Args:
+        df (pd.DataFrame): The input DataFrame containing series data.
+        iso_3_map (dict): A dictionary mapping alpha-3 country codes to their corresponding names.
+
+    Returns:
+        pd.DataFrame: The transformed DataFrame with structured and canonical series data.
+    """
+    # Normalize and rename indicator columns
+    df_indicator = pd.json_normalize(df["indicator"])
+    df_indicator.rename(
+        columns={"id": "series_id", "value": "series_name"}, inplace=True
+    )
+    # Normalize and rename country columns
+    df_country = pd.json_normalize(df["country"])
+    df_country.rename(
+        columns={"id": "alpha_3_code", "value": "country_name"}, inplace=True
+    )
+
+    # Concatenate normalized dataframes with original dataframe
+    df = pd.concat(
+        [df.drop(columns=["indicator", "country"]), df_indicator, df_country], axis=1
+    )
+
+    # Rename primary columns using predefined mapping
+    df.rename(columns=PRIMARY_COLUMNS_TO_RENAME, inplace=True)
+
+    # Apply value handling to transform values and value labels
+    df[["value", SERIES_PROPERTY_PREFIX + "value_label"]] = df.apply(
+        handle_value, axis=1, result_type="expand"
+    )
+
+    # Select columns with specific prefixes that are not in canonical names
+    to_select_columns = [
+        col
+        for col in df.columns
+        if any(
+            [
+                col.startswith(DIMENSION_COLUMN_PREFIX),
+                col.startswith(SERIES_PROPERTY_PREFIX),
+            ]
+        )
+        and col not in CANONICAL_COLUMN_NAMES
+    ]
+
+    # Assign source URL to the DataFrame
+    df["source"] = BASE_URL
+
+    # Filter and organize dataframe columns
+    df = df[CANONICAL_COLUMN_NAMES + to_select_columns]
+
+    # Convert year to integer type
+    try:
+        df["year"] = df["year"].astype(int)
+    except ValueError:
+        logging.warning("Could not convert year to integer type")
+
+    # Replace empty strings with None
+    df.replace({"": None}, inplace=True)
+
+    # Filter rows based on alpha_3_code presence in iso_3_map
+    df = df.loc[df["alpha_3_code"].isin(iso_3_map.keys())].reset_index(drop=True)
+
+    # Ensure canonical columns and sort them
+    df = ensure_canonical_columns(df)
+    df = sort_columns_canonically(df)
+
+    # Assert no duplicate rows exist after transformation
+    assert (
+        df.drop("value", axis=1).duplicated().sum() == 0
+    ), exceptions.DUPLICATE_ERROR_MESSAGE
+
+    return df
