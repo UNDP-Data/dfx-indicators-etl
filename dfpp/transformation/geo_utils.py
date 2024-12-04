6aaf7f7e1771080079ebcfe70db929c4bc14d793
mykhailo.slukvin <mykhailo.slukvin@gmail.com>
2024-12-05 01:56:53 +0200

    feat: add remap of string country names to iso3 for world bank as a geo utility

--- dfpp/transformation/geo_utils.py
+++ dfpp/transformation/geo_utils.py
@@ -7,7 +7,7 @@ import pandas as pd
 
 from dfpp.storage import StorageManager
 
-__all__ = ["get_iso3_to_official_name_map", "get_numeric_to_iso3_map"]
+__all__ = ["get_iso3_to_official_name_map", "get_numeric_to_iso3_map", "get_string_to_iso3_map"]
 
 
 async def get_iso3_to_official_name_map() -> dict:
@@ -16,8 +16,6 @@ async def get_iso3_to_official_name_map() -> dict:
         path = os.path.join(storage_manager.utilities_path, "sep5_country_lookup.xlsx")
         data = await storage_manager.read_blob(path=path)
     df_country_map = pd.read_excel(BytesIO(data), sheet_name="country_lookup")
-    df_country_map = df_country_map[df_country_map["Numeric code"].notna()]
-    df_country_map["Numeric code"] = df_country_map["Numeric code"].astype(int)
     iso_3_country = dict(df_country_map[["iso3", "countryname"]].values)
     return iso_3_country
 
@@ -45,3 +43,14 @@ async def get_numeric_to_iso3_map() -> dict:
 
     iso_3_map = iso_3_country
     return iso_3_map
+
+
+async def get_string_to_iso3_map() -> dict:
+    "get a mapping of string country codes to alpha_3_code codes"
+    async with StorageManager() as storage_manager:
+        path = os.path.join(storage_manager.utilities_path, "sep5_country_lookup.xlsx")
+        data = await storage_manager.read_blob(path=path)
+    df_country_map = pd.read_excel(BytesIO(data), sheet_name="country_lookup")
+    iso_3_country = dict(df_country_map[["countryname", "iso3"]].values)
+    return iso_3_country
+    
