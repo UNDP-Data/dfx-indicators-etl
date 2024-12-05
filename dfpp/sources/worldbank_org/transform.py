64ef4786e9f0297b6a4c578e84a63ae2534f86fa
mykhailo.slukvin <mykhailo.slukvin@gmail.com>
2024-12-05 02:04:07 +0200

    fix: worldbank select column to represent alpha_3_code dynamically based on the data evidence fix: select year rows accounting for integer, float dtypes

--- dfpp/sources/worldbank_org/transform.py
+++ dfpp/sources/worldbank_org/transform.py
@@ -42,10 +42,10 @@ def transform_series(df: pd.DataFrame, iso_3_map: dict) -> pd.DataFrame:
     Args:
         df (pd.DataFrame): The input DataFrame containing series data.
         iso_3_map (dict): A dictionary mapping alpha-3 country codes to their corresponding names.
-
     Returns:
         pd.DataFrame: The transformed DataFrame with structured and canonical series data.
     """
+    to_rename_columns = PRIMARY_COLUMNS_TO_RENAME.copy()
     # Normalize and rename indicator columns
     df_indicator = pd.json_normalize(df["indicator"])
     df_indicator.rename(
@@ -54,7 +54,7 @@ def transform_series(df: pd.DataFrame, iso_3_map: dict) -> pd.DataFrame:
     # Normalize and rename country columns
     df_country = pd.json_normalize(df["country"])
     df_country.rename(
-        columns={"id": "alpha_3_code", "value": "country_name"}, inplace=True
+        columns={"id": "alpha_3_code_id", "value": "country_name"}, inplace=True
     )
 
     # Concatenate normalized dataframes with original dataframe
@@ -62,8 +62,35 @@ def transform_series(df: pd.DataFrame, iso_3_map: dict) -> pd.DataFrame:
         [df.drop(columns=["indicator", "country"]), df_indicator, df_country], axis=1
     )
 
+    # Replace empty strings with None
+    df.replace({"": None}, inplace=True)
+
+    alpha_3_code_candidates = [
+        "countryiso3code",
+        "alpha_3_code_id",
+    ]
+    
+    for col_name in alpha_3_code_candidates:
+        if not df[col_name].isna().all():
+            # if any unique value is alpha2 but not alpha3 code raise ValueError
+            unique_values = df[col_name].unique()
+            if any(len(value) == 2 for value in unique_values if value):
+                raise ValueError(
+                    f"Column {col_name} contains alpha-2 country codes which are not supported."
+                )
+
+            if any(value in iso_3_map.values() for value in unique_values if value):
+                to_rename_columns[col_name] = "alpha_3_code"
+                break
+    
+    if "alpha_3_code" not in to_rename_columns.values():
+        df["alpha_3_code"] = df["country_name"].replace(iso_3_map)
+        
+
     # Rename primary columns using predefined mapping
-    df.rename(columns=PRIMARY_COLUMNS_TO_RENAME, inplace=True)
+    df.rename(columns=to_rename_columns, inplace=True)
+
+    assert "alpha_3_code" in df.columns, "alpha_3_code column not found (has not been assigned)"
 
     # Apply value handling to transform values and value labels
     df[["value", SERIES_PROPERTY_PREFIX + "value_label"]] = df.apply(
@@ -92,18 +119,17 @@ def transform_series(df: pd.DataFrame, iso_3_map: dict) -> pd.DataFrame:
     # Convert year to integer, selecting only convertible rows
     # Leave out rows that have year annotations such as quarters, future targets
     # E.g. `2020Q1`, `2050 Target`
-    df = df[df["year"].apply(lambda x: str(x).isdigit())]
+    df = df[df["year"].apply(lambda x: str(x).isdigit() or isinstance(x, (float, int)))]
     df["year"] = df["year"].astype(int)
 
-    # Replace empty strings with None
-    df.replace({"": None}, inplace=True)
-
     # Filter rows based on alpha_3_code presence in iso_3_map
-    df = df.loc[df["alpha_3_code"].isin(iso_3_map.keys())].reset_index(drop=True)
+    df = df.loc[df["alpha_3_code"].isin(iso_3_map.values())].reset_index(drop=True)
 
     # Ensure canonical columns and sort them
     df = ensure_canonical_columns(df)
     df = sort_columns_canonically(df)
+
+    assert df["value"].notna().any(), "All values are null"
 
     # Assert no duplicate rows exist after transformation
     assert (
