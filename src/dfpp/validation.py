e217533ec9b47328fc0175049845e45650926ff9
Mykola Skrynnyk <45297092+mykolaskrynnyk@users.noreply.github.com>
2025-09-10 11:21:02 +0200

    Refactor(validation): extract a function to count duplicates

--- src/dfpp/validation.py
+++ src/dfpp/validation.py
@@ -4,12 +4,37 @@ Validation function to ensure data integrity and consistency.
 
 import pandas as pd
 
-__all__ = ["check_duplicates"]
+__all__ = ["count_duplicates", "check_duplicates"]
+
+
+def count_duplicates(df: pd.DataFrame) -> int:
+    """
+    Count duplicates in a canonical data frame.
+
+    Parameters
+    ----------
+    df : pd.DataFrame
+        Canonical data frame.
+
+    Returns
+    -------
+    int
+        Count of duplicates in the data frame.
+    """
+    panel_columns = ["alpha_3_code", "series_id", "year"]
+    disaggregation_columns = df.filter(like="disagr_", axis=1).columns.tolist()
+    columns = panel_columns + disaggregation_columns
+    return df.reindex(columns=columns).duplicated().sum()
 
 
 def check_duplicates(df: pd.DataFrame) -> None:
     """
     Check if the canonical data frame contains any duplicates.
+
+    Parameters
+    ----------
+    df : pd.DataFrame
+        Canonical data frame.
 
     Returns
     -------
@@ -21,7 +46,4 @@ def check_duplicates(df: pd.DataFrame) -> None:
     AssertionError
         If any duplicates found.
     """
-    panel_columns = ["alpha_3_code", "series_id", "year"]
-    disaggregation_columns = df.filter(like="disagr_", axis=1).columns.tolist()
-    columns = panel_columns + disaggregation_columns
-    assert (n := df.duplicated(subset=columns).sum()) == 0, f"{n:,} duplicates found."
+    assert (n := count_duplicates(df)) == 0, f"{n:,} duplicates found."
