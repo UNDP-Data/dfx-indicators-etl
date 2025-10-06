38ad6e1fa0aef7f886a1d2bb163168fecfbe3a5e
Mykola Skrynnyk <45297092+mykolaskrynnyk@users.noreply.github.com>
2025-10-06 18:51:50 +0200

    Feat(storage): add a function to infer the storage type

--- src/dfpp/storage/__init__.py
+++ src/dfpp/storage/__init__.py
@@ -2,7 +2,26 @@
 Storage interfaces for I/O operations.
 """
 
+import os
+
+from ._base import BaseStorage
 from .azure import AzureStorage
 from .local import LocalStorage
 
-__all__ = ["AzureStorage", "LocalStorage"]
+__all__ = ["AzureStorage", "LocalStorage", "get_storage"]
+
+
+def get_storage(**kwargs) -> BaseStorage:
+    """
+    Utility function to get appropriate Storage class based on environment variables.
+
+    Returns
+    -------
+    BaseStorage
+        Storage class.
+    """
+    if os.getenv("AZURE_STORAGE_CONTAINER_NAME") is not None:
+        storage = AzureStorage(**kwargs)
+    else:
+        storage = LocalStorage(**kwargs)
+    return storage
