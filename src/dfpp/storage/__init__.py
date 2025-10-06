1e85d7b9380fe48df82ce7d3a79eba40c0408053
Mykola Skrynnyk <45297092+mykolaskrynnyk@users.noreply.github.com>
2025-10-06 18:05:34 +0200

    Chore(src): remove a legacy storage interface for Azure Storage

--- src/dfpp/storage/__init__.py
+++ src/dfpp/storage/__init__.py
@@ -1,2 +1,8 @@
-from .azure import *
+"""
+Storage interfaces for I/O operations.
+"""
+
+from .azure import AzureStorage
 from .local import LocalStorage
+
+__all__ = ["AzureStorage", "LocalStorage"]
