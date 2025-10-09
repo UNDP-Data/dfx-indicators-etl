06404f4366d6e5de87acde5bd007fc02e2ade4ad
Mykola Skrynnyk <45297092+mykolaskrynnyk@users.noreply.github.com>
2025-10-09 13:41:42 +0200

    Refactor(pipelines): use ETL classes for the IMF

---
+++ src/dfpp/sources/imf_org.py
@@ -0,0 +1,136 @@
+"""
+ETL components to process data from the IMF DataMapper API.
+See https://www.imf.org/external/datamapper/api/help.
+"""
+
+import httpx
+import pandas as pd
+from pydantic import Field, HttpUrl
+from tqdm import tqdm
+
+from ._base import BaseRetriever, BaseTransformer
+
+
+__all__ = ["Retriever", "Transformer"]
+
+
+class Retriever(BaseRetriever):
+    uri: HttpUrl = Field(
+        default="https://www.imf.org/external/datamapper/api/v1/",
+        frozen=True,
+        validate_default=True,
+    )
+
+    def __call__(self, **kwargs) -> pd.DataFrame:
+        """
+        Retrieve data from the the IMF DataMapper API,
+
+        Parameters
+        ----------
+        **kwargs
+            Extra arguments to pass to `_get_data`.
+
+        Returns
+        -------
+        pd.DataFrame
+            Raw data from the API for the indicators with supported disaggregations.
+        """
+        df_metadata = self._get_metadata()
+        indicator_codes = df_metadata["indicator_code"].tolist()
+        data = []
+        with self.client as client:
+            for indicator_code in tqdm(indicator_codes):
+                df = self._get_data(indicator_code, client=client, **kwargs)
+                if df is None:
+                    continue
+                data.append(df)
+        df_data = pd.concat(data, axis=0, ignore_index=True)
+        df_data = df_data.merge(df_metadata, how="left", on="indicator_code")
+        return df_data
+
+    def _get_metadata(self) -> pd.DataFrame:
+        """
+        Get series metadata from the IMF DataMapper API indicators endpoint.
+
+        Returns
+        -------
+        pd.DataFrame
+            Data frame with twthree columns: `indicator_code`, `indicator_name` and `unit`.
+        """
+        response = self.client.get("indicators")
+        response.raise_for_status()
+        data = response.json()
+        data = [
+            {"series_id": series_id} | metadata
+            for series_id, metadata in data["indicators"].items()
+            if series_id
+        ]
+        columns = {
+            "series_id": "indicator_code",
+            "label": "indicator_name",
+            "unit": "unit",
+        }
+        return pd.DataFrame(data).reindex(columns=columns).rename(columns=columns)
+
+    def _get_data(
+        self,
+        indicator_code: str,
+        client: httpx.Client,
+        start_period: int = 1950,
+        end_period: int = 2050,
+        **kwargs,
+    ) -> pd.DataFrame | None:
+        """
+        Get series data from the IMF DataMapper API endpoint.
+
+        Parameters
+        ----------
+        indicator_code : str
+            Indicator code to retrieve data for. See `_get_metadata`.
+            Retrieve data until this date.
+        client : httpx.Client,
+            Client with a `base_url` to use for making an HTTP GET request.
+
+        Returns
+        -------
+        pd.DataFrame or None
+            Data frame with raw data as returned by the API or None.
+        """
+        params = {
+            "periods": ",".join(map(str, range(start_period, end_period)))
+        } | kwargs
+        if client.base_url is None:
+            raise ValueError("`client` must include a `base_url`.")
+        response = client.get(indicator_code, params=params)
+        response.raise_for_status()
+        data = response.json()
+        if (values := data.get("values")) is None:
+            return None
+        dfs = []
+        for country_code, records in values[indicator_code].items():
+            df = pd.DataFrame(records.items(), columns=["year", "value"])
+            df["country_code"] = country_code
+            df["indicator_code"] = indicator_code
+            dfs.append(df)
+        return pd.concat(dfs, axis=0, ignore_index=True)
+
+
+class Transformer(BaseTransformer):
+
+    def __call__(self, df: pd.DataFrame, **kwargs) -> pd.DataFrame:
+        """
+        Tranform raw data from the IMF DataMapper API.
+
+        No transformation is necessary for this source.
+
+        Parameters
+        ----------
+        df : pd.DataFrame
+            Raw data as returned by the retrieval section.
+
+        Returns
+        -------
+        pd.DataFrame
+            Standardised data frame.
+        """
+        return df
--- src/dfpp/sources/imf_org/__init__.py
+++
@@ -1,8 +0,0 @@
-"""
-ETL routines for data from the International Monetary Fund (IMF).
-"""
-
-from .retrieve import *
-from .transform import *
-
-SOURCE_NAME = "imf_org"
