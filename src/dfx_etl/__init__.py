d1a00e02686ebd7f2b23b338aeb5e75a730ce26e
Mykola Skrynnyk <45297092+mykolaskrynnyk@users.noreply.github.com>
2025-10-10 15:54:24 +0200

    Docs: bump the package version

--- src/dfx_etl/__init__.py
+++ src/dfx_etl/__init__.py
@@ -4,4 +4,4 @@ An ETL pipeline for managing indicator data for the Data Futures Exchange (DFx).
 
 from . import pipelines, storage, utils, validation
 
-__version__ = "0.1.0"
+__version__ = "1.0.0b0"
