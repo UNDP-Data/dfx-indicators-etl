62788003335cb2f33bfe89d7c24b7dcac961cd02
Mykola Skrynnyk <45297092+mykolaskrynnyk@users.noreply.github.com>
2025-12-26 15:31:55 +0100

    Feat(pipelines): add a pipeline for the WDI data

---
+++ src/dfx_etl/pipelines/world_bank_wdi.py
@@ -0,0 +1,93 @@
+"""
+ETL components to process data from the World Development Indicators by  the World Bank.
+See https://datatopics.worldbank.org/world-development-indicators/.
+"""
+
+import logging
+from pathlib import Path
+
+import pandas as pd
+from pydantic import Field
+
+from ..exceptions import StorageRequiredError
+from ..storage import BaseStorage
+from ._base import BaseRetriever, BaseTransformer
+
+__all__ = ["Retriever", "Transformer"]
+
+logger = logging.getLogger(__name__)
+
+
+class Retriever(BaseRetriever):
+    """
+    A class for retrieving data from the WDI database.
+
+    Use bulk download to manually obtain the data first.
+    """
+
+    uri: Path = Field(
+        default="inputs/WDI_CSV/WDICSV.csv",
+        frozen=True,
+        validate_default=True,
+    )
+
+    def __call__(self, **kwargs) -> pd.DataFrame:
+        """
+        Retrieve data from the WDI database files.
+
+        Parameters
+        ----------
+        **kwargs
+            Extra arguments to pass to `pd.read_*` function.
+
+        Returns
+        -------
+        pd.DataFrame
+            Raw data frame with the data from the databae.
+        """
+        if (storage := kwargs.pop("storage", None)) is None:
+            raise StorageRequiredError
+        file_path = storage.join_path(self.uri)
+        return storage.read_dataset(file_path, **kwargs)
+
+    def _get_metadata(self, storage: BaseStorage) -> pd.DataFrame:
+        """
+        Get the metadata file.
+        """
+        file_path = storage.join_path(self.uri.with_name("WDISeries.csv"))
+        return storage.read_dataset(file_path)
+
+
+class Transformer(BaseTransformer):
+    """
+    A class for transforming raw data from the World Bank Indicator API.
+    """
+
+    def transform(self, df: pd.DataFrame, **kwargs) -> pd.DataFrame:
+        """
+        Transform data from the WDI dataset.
+
+        Parameters
+        ----------
+        df : pd.DataFrame
+            Raw data frame.
+
+        Returns
+        -------
+        pd.DataFrame
+            Transformed data frame in the canonical format.
+        """
+        columns = {
+            "Country Name": "country_name",
+            "Country Code": "country_code",
+            "Indicator Name": "indicator_name",
+            "Indicator Code": "indicator_code",
+        }
+        df = df.melt(id_vars=list(columns), var_name="year", value_name="value")
+        df["year"] = df["year"].astype(int)
+        df = df.query("year >= 2015").dropna(subset=["value"])
+        df.rename(columns=columns, inplace=True)
+        df["indicator_name"] = df.apply(
+            lambda row: f"{row.indicator_name} [{row.indicator_code}]", axis=1
+        )
+        return df
